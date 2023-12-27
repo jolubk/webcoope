f4f1d9c7090c95b870bb375ab02e585eb5b1732b
MATEO GONZALES IBARGUEN <kyori2020@gmail.com>
2023-12-27 09:34:17 -0500

    Retiro de navicoop, solo comentado banner y menu campanias

--- menucampanas.php
+++ menucampanas.php
@@ -2,7 +2,7 @@
     <h2>Campañas</h2>
     <div class="lista-enlace" id="nosotros" style="display:block; padding-top:0;">
         <a href="promociones.php">SUPER AHORRO</a>
-        <a href="navicoop.php">NAVICOOP</a>
+        <!-- <a href="navicoop.php">NAVICOOP</a> -->
         <a href="cuentacts.php">CUENTA CTS</a>
     </div>
 </div>
